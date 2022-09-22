40cce2e52c3221a640bbec27d25c59cab08ca181
niteshpatond <nitesh.patond96@gmail.com>
2022-09-22 22:13:13 +0530

    "Adding Project Structure"

---
+++ Hotel_Manangement/src/main/java/com/nitesh/HotelManangementApplication.java
@@ -0,0 +1,13 @@
+package com.nitesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManangementApplication.class, args);
+	}
+
+}
---
+++ Hotel_Manangement/src/test/java/com/nitesh/HotelManangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
